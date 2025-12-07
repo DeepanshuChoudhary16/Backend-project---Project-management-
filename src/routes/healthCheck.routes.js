d2f14ab240e26999edc3c7afe539510c281b8482
Deepanshu Choudhary <deepanshuchoudhary162002@gmail.com>
2025-12-07 16:12:29 +0530

    Save the file

--- src/routes/healthCheck.routes.js
+++ src/routes/healthCheck.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { healthCheck } from "../controllers/healthCheck.controller.js";
 
-const router = Router()
+const router = Router();
 
-router.route("/").get(healthCheck)
+router.route("/").get(healthCheck);
 export default router;
