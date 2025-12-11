1a10c1a19d672ba6add28fc08ccb76a120f1df51
Deepanshu Choudhary <deepanshuchoudhary162002@gmail.com>
2025-12-11 16:12:49 +0530

    add mail.js for mail formation and send the mail

---
+++ src/utils/mail.js
@@ -0,0 +1,75 @@
+import Mailgen from "mailgen";
+import nodemailer from "nodemailer";
+
+const sendEmail = async (options) => {
+  new Mailgen({
+    theme: "default",
+    product: {
+      name: "Task Manager",
+      link: "https://taskmanagelink.com",
+    },
+  });
+
+  const emailTextual = mailGenerator.generatePlaintext(option.mailgenContent);
+  const emailHTML = mailGenerator.generate(option.mailgenContent);
+
+  const transpoter = nodemailer.createTransport({
+    host: process.env.MAILTRAP_SMTP_HOST,
+    port: process.env.MAILTRAP_SMTP_PORT,
+    auth: {
+      user: process.env.MAILTRAP_SMTP_USER,
+      pass: process.env.MAILTRAP_SMTP_PASS,
+    },
+  });
+
+  const mail = {
+    from: "mail.taskmanager@example.com",
+    to: options.email,
+    subject: options.subject,
+    text: emailTextual,
+    html: emailHTML,
+  };
+  try {
+    await transpoter.sendMail(mailgenContent);
+  } catch (error) {
+    console.error("Email service failed siliendly");
+  }
+};
+
+const emailVerificationMailgen = (username, verificationUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "Welcome to our App! we' are excited to have you on board.",
+      action: {
+        instructions: "To verify your email please click on the button",
+        button: {
+          color: "#22BC66",
+          text: "Verify your email",
+          link: verificationUrl,
+        },
+      },
+      outro: "Need Help, or have question? Just reply to E-mail",
+    },
+  };
+};
+
+const forgotPasswordMailgen = (username, passwordResetUrl) => {
+  return {
+    body: {
+      name: username,
+      intro: "We got a request to reset the password of your account",
+      action: {
+        instructions: "To reset the password",
+        button: {
+          color: "#da3e1fff",
+          text: "Reset password",
+          link: passwordResetUrl,
+        },
+      },
+      outro: "Need Help, or have question? Just reply to E-mail",
+    },
+  };
+};
+
+export { emailVerificationMailgen, forgotPasswordMailgen,sendEmail };
